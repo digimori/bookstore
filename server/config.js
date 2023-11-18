f55acbf501dc469aa33d7bf21c3d0a253e4eedeb
digimori <hanaorikuu@gmail.com>
2023-11-18 18:42:23 +0000

    Mongoose installed. MongoDB connection established.

--- server/config.js
+++ server/config.js
@@ -1,1 +1,5 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://digimori:Flayermind1!@bookstoredatabase.th0cq6g.mongodb.net/bookstorestorage?retryWrites=true&w=majority";
+// bookstorestorage within the above string is the collection name
--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
+// Http route connection:
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send(`Request completed!`);
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening on port: ${PORT}`);
-});
+// Database connection
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    // try block for success
+    console.log(`Success, connection to database established.`);
+    // Port listener
+    app.listen(PORT, () => {
+      console.log(`App is listening on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    // error block
+    console.log(`Cannot connect to database, Error: ${error}`);
+  });
