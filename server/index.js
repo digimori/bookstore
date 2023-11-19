f6801cb7033cb533087484ad61059923daf80148
digimori <hanaorikuu@gmail.com>
2023-11-19 21:28:01 +0000

    CRUD functionality routes added to server side - Tested with Postman to verify.

--- server/index.js
+++ server/index.js
@@ -60,6 +60,66 @@ app.get("/books", async (request, response) => {
   }
 });
 
+// Http route to retrieve single entries via ID:
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const singleBook = await Book.findById(id);
+    return response.status(200).json(singleBook);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Http route to Update a book (PUT method):
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publicationDate ||
+      !request.body.description
+    ) {
+      return response.status(400).send({
+        message:
+          "Please send all required fields: title, author, publicationDate, description.",
+      });
+    }
+    const { id } = request.params;
+    const bookUpdate = await Book.findByIdAndUpdate(id, request.body); // Pulling the id and passing the results of the request.body
+    if (!bookUpdate) {
+      return response.status(404).json({ message: "Book Entry not found." });
+    }
+    return response
+      .status(200)
+      .send({ message: "Book entry updated successfully." });
+  } catch (error) {
+    console.log(`Could not update entry, Error message: ${error.message}`);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Http Route to Delete the book entry
+app.delete("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const bookDelete = await Book.findByIdAndDelete(id); // Pulling the id and passing the results of the request.body
+
+    if (!bookDelete) {
+      return response
+        .status(404)
+        .json({ message: "Book Entry has not been deleted" });
+    }
+    return response
+      .status(200)
+      .send({ message: "Book entry successfully deleted." });
+  } catch (error) {
+    console.log(`Could not delete entry, Error message: ${error.message}`);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 // Database connection
 mongoose
   .connect(mongoDBURL)
