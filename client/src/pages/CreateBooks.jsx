aa170f608ec614b0f37b8a075b8c70a6cc901977
digimori <hanaorikuu@gmail.com>
2023-11-20 19:29:37 +0000

    CreateBook.jsx form created.

--- client/src/pages/CreateBooks.jsx
+++ client/src/pages/CreateBooks.jsx
@@ -8,6 +8,7 @@ const CreateBooks = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [publicationDate, setPublicationDate] = useState(""); // This we could probably consider turning into a date picker later.
+  const [description, setDescription] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate(); // Re-Routes after creating a book
 
@@ -16,16 +17,17 @@ const CreateBooks = () => {
       title,
       author,
       publicationDate,
+      description,
     };
     setLoading(true);
     axios
       .post(
-        `https://5555-digimori-bookstore-6c00cvdc0rz.ws-eu106.gitpod.io/books/${id}`,
+        "https://5555-digimori-bookstore-6c00cvdc0rz.ws-eu106.gitpod.io/books",
         data
-      ) // Second parameter is the data posted
+      )
       .then(() => {
-        setLoading(false); // Stops data moving once sent
-        navigate("/"); // Returns to home page after posting
+        setLoading(false);
+        navigate("/");
       })
       .catch((error) => {
         setLoading(false);
@@ -36,7 +38,58 @@ const CreateBooks = () => {
       });
   };
 
-  return <div>CreateBooks</div>;
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3cl my-4">Create Book Entry:</h1>
+      {loading ? <Loader /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-cl mr-4 text-gray-500">Title:</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+
+        <div className="my-4">
+          <label className="text-cl mr-4 text-gray-500">Author:</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+
+        <div className="my-4">
+          <label className="text-cl mr-4 text-gray-500">
+            Publication Date:
+          </label>
+          <input
+            type="text"
+            value={publicationDate}
+            onChange={(e) => setPublicationDate(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-cl mr-4 text-gray-500">Description:</label>
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <button className="p-2 bk-sky-300 m-8" onClick={handleSaveBook}>
+          Save Entry
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CreateBooks;
