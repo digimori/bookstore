7c7b0f82bc4da0fa199518dfbbcf6da57c998300
digimori <hanaorikuu@gmail.com>
2023-11-20 19:08:03 +0000

    CreateBook functionality added. Not yet displayed as JSX

--- client/src/pages/CreateBooks.jsx
+++ client/src/pages/CreateBooks.jsx
@@ -1,9 +1,42 @@
-
+import React, { useState, useEffect } from "react";
+import BackButton from "../components/BackButton";
+import Loader from "../components/Loader";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const CreateBooks = () => {
-  return (
-    <div>CreateBooks</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publicationDate, setPublicationDate] = useState(""); // This we could probably consider turning into a date picker later.
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate(); // Re-Routes after creating a book
 
-export default CreateBooks
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publicationDate,
+    };
+    setLoading(true);
+    axios
+      .post(
+        `https://5555-digimori-bookstore-6c00cvdc0rz.ws-eu106.gitpod.io/books/${id}`,
+        data
+      ) // Second parameter is the data posted
+      .then(() => {
+        setLoading(false); // Stops data moving once sent
+        navigate("/"); // Returns to home page after posting
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert(
+          `An Error has occured: ${error}. Please Check console for further information.`
+        );
+        console.log(error);
+      });
+  };
+
+  return <div>CreateBooks</div>;
+};
+
+export default CreateBooks;
