2374713da01d49a635fefcd831879914efe4e6e3
digimori <hanaorikuu@gmail.com>
2023-11-20 13:42:59 +0000

    React Routes added for each component page element.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBooks from "./pages/CreateBooks";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import ShowBooks from "./pages/ShowBooks";
+
 const App = () => {
-  return <div className="bg-red-400 text-white">App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBooks />} />
+      <Route path="/books/details/:id" element={<ShowBooks />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ client/src/pages/ShowBooks.jsx
@@ -0,0 +1,5 @@
+const ShowBooks = () => {
+  return <div>ShowBooks</div>;
+};
+
+export default ShowBooks;
