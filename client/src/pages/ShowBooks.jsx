f6c74fbddad3e844df5e0bf0aaea894ecb185e6e
digimori <hanaorikuu@gmail.com>
2023-11-26 15:41:03 +0000

    Turned Book into its own component to clean up ShowBooks.jsx

--- client/src/pages/ShowBooks.jsx
+++ client/src/pages/ShowBooks.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import Loader from "../components/Loader";
+import Book from "../components/Book";
+
 
 const ShowBook = () => {
   const [books, setBooks] = useState({});
@@ -32,32 +34,7 @@ const ShowBook = () => {
       {loading ? (
         <Loader />
       ) : (
-        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
-          <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">Id</span>
-            <span>{books._id}</span>
-          </div>
-          <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">Title</span>
-            <span>{books.title}</span>
-          </div>
-          <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">Author</span>
-            <span>{books.author}</span>
-          </div>
-          <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">Publish Year</span>
-            <span>{books.publicationDate}</span>
-          </div>
-          <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">Create Time</span>
-            <span>{new Date(books.createdAt).toString()}</span>
-          </div>
-          <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">Last Update Time</span>
-            <span>{new Date(books.updatedAt).toString()}</span>
-          </div>
-        </div>
+        <Book books={books} />
       )}
     </div>
   );
